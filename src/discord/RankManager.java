701070f13ff0d0a67754921c5c92969abf4966e6
Austin <mrfrozenfirefly@gmail.com>
2018-03-20 00:05:03 -0700

    rank name changes

--- src/discord/RankManager.java
+++ src/discord/RankManager.java
@@ -11,12 +11,12 @@ public class RankManager {
     
     //hardcoded ranks
     private final static Rank[] RANKS = {
-        new Rank(386829087139692556L, "Guest", 0),
-        new Rank(386829020161114112L, "Casual", 10),
-        new Rank(275486798699036683L, "Regular", 20),
-        new Rank(386829239409704961L, "Addict", 30),
-        new Rank(387908458382491650L, "Hero", 40),
-        new Rank(387908362450370560L, "Legend", 50),
+        new Rank(386829087139692556L, "Peasant", 0),
+        new Rank(386829020161114112L, "Citizen", 10),
+        new Rank(275486798699036683L, "Knight", 20),
+        new Rank(386829239409704961L, "Hero", 30),
+        new Rank(387908458382491650L, "Lord", 40),
+        new Rank(387908362450370560L, "King", 50),
         new Rank(342028348886614016L, "Ancient", 60),
         new Rank(387905623662133249L, "Titan", 70),
         new Rank(386935240729296899L, "God", 80)
