a5e56fdadc54fc7e12ecf6e3731a51930044246e
Austin <mrfrozenfirefly@gmail.com>
2019-01-15 00:36:57 -0800

    Store users in HashMap

--- src/main/java/discord/UserManager.java
+++ src/main/java/discord/UserManager.java
@@ -1,12 +1,10 @@
 package discord;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import discord.object.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
@@ -14,24 +12,22 @@ import sx.blah.discord.handle.obj.IUser;
 
 public class UserManager {
 
-    private static List<User> users;
+    private static HashMap<Long, User> users;
 
-    //Database management
     public static void createDatabase(IGuild guild) {
+        users = new HashMap<>();
         if (new File("users.json").exists()) {
             loadDatabase();
             checkNewUsersInGuild(guild);
             checkRemovedUsersInGuild(guild);
         } else {
             System.out.println("Creating database...");
-            users = new ArrayList<>();
             List<IUser> allUsers = guild.getUsers();
             allUsers.removeIf(user -> user.isBot());
-            for (IUser user : allUsers) {
-                users.add(new User(user.getLongID(), user.getDisplayName(guild)));
-            }
+            allUsers.forEach(user -> {
+                users.put(user.getLongID(), new User(user.getLongID(), user.getDisplayName(guild)));
+            });
             System.out.println("Database created.");
-            saveDatabase();
         }
         validateUsers(guild);
         saveDatabase();
@@ -42,8 +38,10 @@ public class UserManager {
         //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         try {
             System.out.println("Loading database...");
-            users = new ArrayList<>(Arrays.asList(
-                    mapper.readValue(new File("users.json"), User[].class)));
+            User[] usersToLoad = mapper.readValue(new File("users.json"), User[].class);
+            for (User user : usersToLoad) {
+                users.put(user.getDiscordID(), user);
+            }
             System.out.println("Database loaded.");
         } catch (IOException e) {
             System.err.println("Database failed to load with error: " + e);
@@ -54,16 +52,15 @@ public class UserManager {
         ObjectMapper mapper = new ObjectMapper();
         System.out.println("Attempting to save database...");
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("users.json"), users);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("users.json"), users.values());
             System.out.println("Database saved.");
         } catch (IOException e) {
-            System.err.println("Database failed to save with error: ");
-            e.printStackTrace();
+            System.err.println("Database failed to save with error: " + e);
         }
     }
 
     public static void validateUsers(IGuild guild) {
-        for (User user : users) {
+        for (User user : users.values()) {
             if (guild.getUserByID(user.getDiscordID()) != null) {
                 RankManager.verifyRoleOnGuild(guild, user);
                 user.getName().verify(guild);
@@ -73,30 +70,22 @@ public class UserManager {
 
     //Check possible users that have left the guild, if so, remove them from the database
     public static void checkRemovedUsersInGuild(IGuild guild) {
-        System.out.println("Checking possible new users to remove from the database...");
-        for (User user : new ArrayList<>(users)) {
-            removeUserIfInvalid(user, guild);
-        }
-        System.out.println("Finished checking possible users to remove.");
+        System.out.println("Checking any possible guild users (that left) to remove from the database...");
+        users.values().removeIf(user -> userIsInvalid(user, guild)); //Won't log when a user is removed, just does it silently
+        System.out.println("Removed any possible users that were invalid");
     }
 
     //Check possible users that are not already in database in guild, add them if found
     private static void checkNewUsersInGuild(IGuild guild) {
         List<IUser> guildUsers = guild.getUsers();
-        boolean foundNewUser = false;
 
-        System.out.println("Checking possible new users to add to the database...");
-
+        System.out.println("Checking possible newly joined guild users to add to the database...");
         for (IUser dUser : guildUsers) {
             if (!dUser.isBot() && !databaseContainsDUser(dUser)) {
                 handleUserJoin(dUser, guild);
-                foundNewUser = true;
             }
         }
         System.out.println("Finished checking possible new guild users.");
-        if (foundNewUser) {
-            saveDatabase();
-        }
     }
 
     public static void handleUserJoin(IUser dUser, IGuild guild) {
@@ -104,24 +93,23 @@ public class UserManager {
             User existingUser = getDBUserFromDUser(dUser);
             RankManager.verifyRoleOnGuild(guild, existingUser);
             existingUser.getName().verify(guild);
-            System.out.println("User joined. Already found " + existingUser.getName().getNick() 
-                    + " in the database.");
-            return;
+            System.out.println("User " + existingUser.getName().getNick() + " joined. Already found them saved in the database.");
+        } else { //They're not in the database, so we add the new user in
+            String name = BotUtils.validateNick(dUser.getDisplayName(guild));
+            //if the name validator returns an empty name, we need a placeholder
+            if (name.isEmpty()) {
+                name = "Realmer";
+            }
+            if (databaseContainsName(name)) {
+                name = getNextAvailableName(name);
+            }
+            User user = new User(dUser.getLongID(), name);
+            users.put(dUser.getLongID(), user);
+            RankManager.verifyRoleOnGuild(guild, user);
+            user.getName().verify(guild);
+            System.out.println("User " + name + " joined. Added them to the database.");
+            saveDatabase();
         }
-        String name = BotUtils.validateNick(dUser.getDisplayName(guild));
-        //if the name validator returns an empty name, we need a placeholder
-        if (name.isEmpty()) {
-            name = "Realmer";
-        }
-        if (databaseContainsName(name)) {
-            name = getNextAvailableName(name);
-        }
-        User user = new User(dUser.getLongID(), name);
-        users.add(user);
-        RankManager.verifyRoleOnGuild(guild, user);
-        user.getName().verify(guild);
-        System.out.println("User joined. Added " + name + " to the database.");
-        saveDatabase();
     }
 
     private static String getNextAvailableName(String name) {
@@ -133,49 +121,32 @@ public class UserManager {
     }
 
     public static void handleUserLeave(IUser dUser, IGuild guild) {
-        //here we are assuming the dUser is in the DB, seems like a bad idea
-        removeUserIfInvalid(getDBUserFromDUser(dUser), guild);
+        if (databaseContainsDUser(dUser)) {
+            removeUserIfInvalid(getDBUserFromDUser(dUser), guild);
+        } else {
+            System.out.println("User " + dUser.getName() + " left the guild, but they weren't in the database anyway.");
+        }
     }
 
     private static void removeUserIfInvalid(User user, IGuild guild) {
         //we keep users that are level 10 and up for now
-        if (user.getProgress().getTotalLevel() < 10 && guild.getUserByID(user.getDiscordID()) == null) {
-            users.remove(user);
+        if (userIsInvalid(user, guild)) {
+            users.remove(user.getDiscordID());
             System.out.println("Removed " + user.getName() + " from the database.");
         }
     }
 
-    private static boolean databaseContainsDUser(IUser dUser) {
-        long ID = dUser.getLongID();
-        for (User user : users) {
-            if (ID == user.getDiscordID()) {
-                return true;
-            }
-        }
-        return false;
+    private static boolean userIsInvalid(User user, IGuild guild) {
+        return (user.getProgress().getTotalLevel() < 10 && guild.getUserByID(user.getDiscordID()) == null);
     }
 
-    public static boolean databaseContainsName(String name) {
-        for (User user : users) {
-            if (user.getName().getNick().toLowerCase().equals(name.toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //Methods for fetching users and ids
+    //Methods for fetching users
     public static User getDBUserFromID(long id) {
-        for (User user : users) {
-            if (user.getDiscordID() == id) {
-                return user;
-            }
-        }
-        return null;
+        return users.get(id);
     }
 
     public static User getDBUserFromName(String name) {
-        for (User user : users) {
+        for (User user : users.values()) { //Crappy solution for this, its just the old arraylist again
             if (user.getName().getNick().equalsIgnoreCase(name)) {
                 return user;
             }
@@ -191,13 +162,13 @@ public class UserManager {
         return getDBUserFromID(message.getAuthor().getLongID());
     }
 
-    public static long getDBUserIDFromName(String name) {
-        for (User user : users) {
-            if (user.getName().getNick().equalsIgnoreCase(name)) {
-                return user.getDiscordID();
-            }
-        }
-        return -1;
+    //Contains checks
+    private static boolean databaseContainsDUser(IUser dUser) {
+        return users.containsKey(dUser.getLongID());
+    }
+
+    public static boolean databaseContainsName(String name) {
+        return (getDBUserFromName(name) != null);
     }
 
 }
--- src/main/java/discord/command/CommandCategory.java
+++ src/main/java/discord/command/CommandCategory.java
@@ -1,11 +1,24 @@
 package discord.command;
 
 public enum CommandCategory {
-    ADMIN, FUN, HIDDEN, INFO, PERK, UTILITY;
+    ADMIN, INFO, PERK, UTILITY, FUN, HIDDEN;
 
     @Override
     public String toString() {
         String temp = super.toString();
-        return temp.charAt(0) + temp.substring(1).toLowerCase();
+        temp = temp.charAt(0) + temp.substring(1).toLowerCase() + " ";
+        switch (this) {
+            case ADMIN:
+                return temp + "\uD83D\uDEE1"; //Shield emoji
+            case INFO:
+                return temp + "📜";
+            case PERK:
+                return temp + "💎";
+            case UTILITY:
+                return temp + "🔧"; 
+            case FUN:
+                return temp + "🎉";           
+        }
+        return "";
     }
 }
